dfd8f3559cc80acc30f367cece5a9013fae7ea8a
mayuridube <49654428+mayuridube@users.noreply.github.com>
2020-06-16 15:10:59 +0530

    Update image_rotation.py

--- image_manipulation/image_rotation.py
+++ image_manipulation/image_rotation.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2 
 import matplotlib.pyplot as plt 
 
-image = cv2.imread('./test_images/index.png') 
+image = cv2.imread('../test_images/index.jpeg') 
 rows,cols = image.shape[:2] 
 
 #(col/2,rows/2) is the center of rotation for the image 
@@ -13,3 +13,4 @@ rows,cols = image.shape[:2]
 M = cv2.getRotationMatrix2D((cols/2,rows/2),90,1) 
 dst = cv2.warpAffine(image,M,(cols,rows)) 
 plt.imshow(dst)
+plt.show()
